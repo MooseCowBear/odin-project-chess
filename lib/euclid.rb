2fc67944b4f6b2a457bbe1a8f15c6429ab42139e
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-12 12:22:01 -0400

    Fix #slope so that it returns nil instead of inifinity for two points in same column

--- lib/euclid.rb
+++ lib/euclid.rb
@@ -1,10 +1,7 @@
 module Euclid
-  def slope(x1, y1, x2, y2)
-    begin 
-      return (y2 - y1) / (x2 - x1)
-    rescue ZeroDivisionError
-      return nil
-    end
+  def slope(x1, y1, x2, y2) #assume board is oriented normal coordinate way, x is col, y is row
+    return nil if (x2.to_f - x1.to_f) == 0
+    return (y2.to_f - y1.to_f) / (x2.to_f - x1.to_f)
   end
 
   def distance(x1, y1, x2, y2)
