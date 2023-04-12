4cd6e2384f207e9f18c21d21acea0963abc3bbb2
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-12 14:45:00 -0400

    Refactor to check is move is on board

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -1,9 +1,11 @@
 require_relative './euclid.rb'
 require_relative './path_checker.rb'
+require_relative './board_boundry.rb'
 
 class Bishop
   include Euclid
   include PathChecker
+  include BoardBoundry
 
   attr_reader :slopes
 
@@ -12,12 +14,16 @@ class Bishop
   end
 
   def valid_move?(board, start_idx, end_idx)
+    correct_move?(board, start_idx, end_idx) && on_board?(board, end_idx)
+  end
+
+  private
+
+  def correct_move?(board, start_idx, end_idx)
     slope = slope(start_idx[1], start_idx[0], end_idx[1], end_idx[0])
     return false if slope.nil?
-    return false unless slopes.include?(slope.abs) 
-
+    return false unless slopes.include?(slope.abs)
     return false unless clear_non_vertical_path?(board, start_idx, end_idx, slope)
-
     true
   end
 end
