2f4df090618242119b2e3fddfcd4ec71b43ceb74
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-12 12:20:46 -0400

    Add #valid_move?, initialize

---
+++ lib/queen.rb
@@ -0,0 +1,23 @@
+require_relative './euclid.rb'
+require_relative './path_checker.rb'
+
+class Queen
+  include Euclid
+  include PathChecker 
+
+  attr_reader :slopes
+
+  def initialize
+    @slopes = Set.new([nil, 1.0, 0.0]) #will check abs of slope
+  end
+
+  def valid_move?(board, start_idx, end_idx)
+    slope = slope(start_idx[1], start_idx[0], end_idx[1], end_idx[0])
+    return false unless slopes.include?(slope) || slopes.include?(slope.abs)
+
+    path_clear = slope.nil? ? clear_vertical_path?(board, start_idx, end_idx) : clear_non_vertical_path?(board, start_idx, end_idx, slope)
+    return false unless path_clear
+
+    true
+  end
+end
