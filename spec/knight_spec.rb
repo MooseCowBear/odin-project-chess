a09fb0c3880a0fc96b560928133f2a7827a28368
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-12 14:37:05 -0400

    Add test with correct slop, wrong distance

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -5,16 +5,16 @@ describe Knight do
     subject(:test_knight) { described_class.new }
     let(:board) { 
       [
-        [nil, nil, nil, nil, nil, nil],
-        [nil, nil, nil, nil, nil, nil],
-        ["x", nil, "K", "x", nil, nil],
-        [nil, nil, "x", nil, nil, nil],
-        [nil, nil, nil, nil, "x", nil],
-        [nil, nil, nil, nil, nil, nil]
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        ["x", nil, "K", "x", nil, nil, nil],
+        [nil, nil, "x", nil, nil, nil, nil],
+        [nil, nil, nil, nil, "x", nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil]
       ]
     }
 
-    context 'when the slope is 2' do
+    context 'when the slope is 2 and distance is correct' do
       it 'returns true' do
         start_pt = [2, 2]
         end_pt = [4, 3]
@@ -23,12 +23,21 @@ describe Knight do
       end
     end
 
-    context 'when the slope is -0.5' do
+    context 'when the slope is -0.5 and distance is correct' do
       it 'returns true' do
         start_pt = [2, 2]
         end_pt = [1, 4]
         res = test_knight.valid_move?(board, start_pt, end_pt)
         expect(res).to be true
+      end
+    end
+
+    context 'when slope is correct but distance is not' do
+      it 'returns false' do
+        start_pt = [2, 2]
+        end_pt = [4, 6]
+        res = test_knight.valid_move?(board, start_pt, end_pt)
+        expect(res).to be false
       end
     end
 
