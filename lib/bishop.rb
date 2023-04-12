838b1e245fe6ac7aed92c2365052465ac8990cae
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-12 14:23:01 -0400

    Fix valid_move? so that it handles nil correctly

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -6,14 +6,15 @@ class Bishop
   include PathChecker
 
   attr_reader :slopes
-  
+
   def initialize
     @slopes = Set.new([1.0])
   end
 
   def valid_move?(board, start_idx, end_idx)
     slope = slope(start_idx[1], start_idx[0], end_idx[1], end_idx[0])
-    return false unless slopes.include?(slope) || slopes.include?(slope.abs)
+    return false if slope.nil?
+    return false unless slopes.include?(slope.abs) 
 
     return false unless clear_non_vertical_path?(board, start_idx, end_idx, slope)
 
