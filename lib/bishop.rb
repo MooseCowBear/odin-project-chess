57d3cfe9fd09527503823366a91885fc1252e4f5
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-12 13:30:45 -0400

    Add bishop class

---
+++ lib/bishop.rb
@@ -0,0 +1,22 @@
+require_relative './euclid.rb'
+require_relative './path_checker.rb'
+
+class Bishop
+  include Euclid
+  include PathChecker
+
+  attr_reader :slopes
+  
+  def initialize
+    @slopes = Set.new([1.0])
+  end
+
+  def valid_move?(board, start_idx, end_idx)
+    slope = slope(start_idx[1], start_idx[0], end_idx[1], end_idx[0])
+    return false unless slopes.include?(slope) || slopes.include?(slope.abs)
+
+    return false unless clear_non_vertical_path?(board, start_idx, end_idx, slope)
+
+    true
+  end
+end
