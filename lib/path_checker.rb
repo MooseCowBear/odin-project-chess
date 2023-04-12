0fbd5cdbe11ce002eb60f9c0c4a7d4bb229a2ecb
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-12 11:18:27 -0400

    Add method to order points so can handle any relation between start and end indices

--- lib/path_checker.rb
+++ lib/path_checker.rb
@@ -1,5 +1,7 @@
 module PathChecker
-  def clearNonVerticalPath?(board, start_idx, end_idx, slope)
+  def clear_non_vertical_path?(board, start_idx, end_idx, slope)
+    start_idx, end_idx = order_non_vert(start_idx, end_idx)
+
     y1, x1 = start_idx
     y2, x2 = end_idx
 
@@ -10,7 +12,10 @@ module PathChecker
     true
   end
 
-  def clearVerticalPath?(board, start_idx, end_idx)
+  def clear_vertical_path?(board, start_idx, end_idx)
+    #only works in one direction !
+    start_idx, end_idx = order_vert(start_idx, end_idx)
+
     y1, x1 = start_idx
     y2, x2 = end_idx
 
@@ -18,5 +23,17 @@ module PathChecker
       return false unless board[interm_y][x1].nil?
     end
     true
+  end
+
+  def order_non_vert(start_idx, end_idx)
+    #x1 needs to be smaller than x2 for nonvert
+    return [end_idx, start_idx] if end_idx[1] < start_idx[1]
+    [start_idx, end_idx]
+  end
+
+  def order_vert(start_idx, end_idx)
+    #y1 needs to be smaller than y2
+    return [end_idx, start_idx] if end_idx[0] < start_idx[0]
+    [start_idx, end_idx]
   end
 end
