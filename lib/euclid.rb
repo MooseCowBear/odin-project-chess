cf8d150eee19a444d16113a700b75ca2a05144ed
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-12 07:44:43 -0400

    Add #slope, #distance

---
+++ lib/euclid.rb
@@ -0,0 +1,13 @@
+module Euclid
+  def slope(x1, x2, y1, y2)
+    begin 
+      return (y2 - y1) / (x2 - x1)
+    rescue ZeroDivisionError
+      return nil
+    end
+  end
+
+  def distance(x1, x2, y1, y2)
+    Math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+  end
+end
