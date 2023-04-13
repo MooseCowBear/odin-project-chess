669792b3dddcb95de08d013f0060102c2c680f36
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-13 17:31:16 -0400

    add methods and helper methods to determine legal moves

--- lib/chess.rb
+++ lib/chess.rb
@@ -9,20 +9,25 @@ require_relative './pawn.rb'
 class Chess
   include Euclid
 
+  attr_accessor :player_white, :player_black, 
+    :board, :turn_white, :white_king_position, 
+    :black_king_position, :num_moves, :en_passant, 
+    :check_white, :check_black, :history
+
   def initialize
     @player_white = nil #get player names at the start of play
     @player_black = nil
     @board = get_starting_board #right now its a new game board
-    #@white_pieces = get_starting_pieces(white) #sets of pieces on the board and adds to relevant collection - used to check for check, checkmate
-    #@black_pieces = get_starting_pieces(black)  #so we don't have to loop the whole board when there are few pieces
     @turn_white = true
-    @white_king_position = nil 
-    @black_king_position = nil 
+    @white_king_position = [7, 4]
+    @black_king_position = [0, 4] 
 
     @num_moves = 0
-    @en_passant = [] #empty array if not an option. [start, end] if option
+    @en_passant = nil #[start, end] if option
     @check_white = false
     @check_black = false
+
+    @winner = nil
     @history = get_history
   end
 
@@ -68,7 +73,28 @@ class Chess
     Hash.new
   end
 
+  def display_turns
+    take_turn until !winner.nil? || num_moves >= 75
+  end
+
+  def take_turn
+
+    #start turn by checking for check. 
+
+    #get appropriate moves and store
+
+    #announce checkmate or stalemate if applicable
+
+    #then get move and validate
+
+    #make move (updating board)
+
+    #update whose move it is
+
+  end
+
   def get_move
+
   end
 
   def convert_move(move)
@@ -93,35 +119,121 @@ class Chess
     pp board #change later
   end
 
-  #need to have functions that test the waters for case when moving a piece out of its position might put player in check
+ 
   def make_move(start_pt, end_pt)
     
   end
 
-  def revert_move(start_pt, end_pt) 
-    make_move(end_pt, start_pt)
+  def move_legal?(move_start, move_end, player_check, check_moves, non_check_moves, available_castles)
+    if player_check
+      return true check_moves[move_start].include?(move_end)
+    else
+      return true if available_castles[move_start] == move_end 
+      return true if non_check_moves.include?(move_start) && board[move_start[0]][move_start[1]].valid_move?(board, move_start, move_end)
+    end
+    false
+  end
+
+  def get_pieces_that_can_move(player_color)
+    pieces_that_can_move = [] #holds their positions
+    board.each_with_index do |row, m| 
+      row.each_with_index do |piece, n|
+        next if piece.nil? || piece.color != player_color
+        get_test_board(m, n)
+        king_position = player_color == "white" ? white_king_position : black_king_position
+        pieces_that_check = checked_by(player_color, king_position)
+        revert_board(piece, m, n)
+        pieces_that_can_move << [m, n] if pieces_that_check.empty? && can_move?(piece, m, n)
+      end
+    end
+    pieces_that_can_move
+  end
+
+  def get_test_board(m, n)
+    board[m, n] = nil
+  end
+
+  def revert_board(piece, m, n)
+    board[m][n] = piece
+  end
+
+  def can_move?(piece, m, n) #pawn needs an extra check
+    return true unless piece.is_a?(Pawn)
+    row = piece.color == "white" ? m - 1 : m + 1
+    (piece.valid_move?(board, [m, n], [row, n]) || 
+    piece.valid_move(board, [m, n], [row, n + 1]) || 
+    piece.valid_move(board, [m, n], [row, n - 1]) || 
+    (!en_passant.nil? && en_passant[0] == [m, n]))
+  end
+
+  def available_castles(king_position, king_color)
+    castles = Hash.new 
+    return castles if board[king_position[0]][king_position[1]].moved
+
+    if board[king_position[0]][0].is_a?(Rook) && !board[king_position[0]][0].moved
+      option = check_castle_option([king_position[0], 0], king_position, king_color)
+      castles[king_position] = [king_position[0], king_position[1] - 1] if option #king moves -2 cols rook goes to right of king which is -2 + 1
+    end
+
+    if board[king_position[0]][7].is_a?(Rook) && !board[king_position[0]][7].moved
+      option = check_castle_option([king_position[0], 7], king_position, king_color)
+      castles[king_position] = [king_position[0], king_position[1] + 1] if option #king moves +2, rook goes to left of king which is +2 - 1
+    end
+    castles
+  end
+
+  def check_castle_option(rook_position, king_position, king_color)
+    min = rook_position[1] < king_position[1] ? rook_position[1] : king_position[1]
+    max = rook_position[1] < king_position[1] ? king_position[1] : rook_position[1]
+
+    min.upto(max) do |col|
+      checked = checked_by(king_color, [king_position[0], col])
+      return false unless checked.empty?
+    end
+    return true
   end
 
 
-  def moves_out_of_check(king_color, king_position, pieces_that_check)
-    #these are 1. moves the king can make without moving to another checked square
-    # 2. moves other pieces can make to "take" the checking opponent piece
+  def moves_from_check(king_color, king_position, pieces_that_check) 
+    #these are:
+    # 1. moves the king can make without moving to another checked square
+    # 2. moves other pieces can make to capture the checking opponent piece
     # 3. moves other pieces can make to get between the king and the checking piece (if checking piece is not a knight)
 
+    if pieces_that_check.length > 1 #more than one opponent piece responsible for check? we can only move the king
+      get_king_moves(king_color, king_position)
+    else
+      king_moves = get_king_moves(king_color, king_position)
+      opponent_position = pieces_that_check[0]
+      opponent_color = king_color == "white" ? "black" : "white"
+      other_moves = moves_that_take_opponent(opponent_color, opponent_position)
+      all_viable_moves = king_moves.merge(other_moves)
+
+      unless board[opponent_position[0]][opponent_position[1]].is_a?(Knight)
+        intercepting_moves = moves_that_intercept_opponent(opponent_color, opponent_position, king_position)
+        all_viable_moves.merge(intercepting_moves) 
+      end
+      
+      #one last possibility: would an en passant move save us?
+      unless en_passant.nil?
+        all_viable_moves[en_passant[0]] << en_passant[1] if en_passant_rescue?(opponent_position)
+      end
+      all_viable_moves
+    end
   end
 
   def checked_by(king_color, king_position)
     #loops board, checking opponent pieces to see if king position is a valid move for any of them
+    #return the posititions of any such pieces
     pieces_that_check = []
     board.each_with_index do |row, m| 
       row.each_with_index do |piece, n|
         next if piece.nil? || piece.color == king_color
-        #for opponent piece
         checks_king = piece.valid_move?(board, [m, n], king_position)
         pieces_that_check << [m, n]
       end
     end
-    pieces_that_check #holds positions of pieces that check
+    pieces_that_check 
   end
 
   def check?(pieces_that_check)
@@ -135,42 +247,49 @@ class Chess
 
   private 
 
-  def king_moves_from_check(king_color, king_position)
+  def get_king_moves(king_color, king_position)
     moves = get_adjacent_positions(king_position)
     viable_moves = Hash.new { |h, k| h[k] = [] }
     moves.each do |move|
       pieces_that_check = checked_by(king_color, move) #check that each move the king can make to see if it is safe
       viable_moves[king_position] << move if pieces_that_check.empty?
     end
-    viable_moves #hash like [from]: [to1, to2, ... ]
+    viable_moves #hash {from king_position] =>  [to1, to2, ... ]}
   end
 
   def moves_that_take_opponent(opponent_color, opponent_position)
-    #if only on opponent piece checks king, possible to get out of check by eating it
-    #so now looking for own pieces that "check" that opponent pieces!
+    #if only one opponent piece checks king, possible to get out of check by capturing it
+    #returns moves that capture the opponent causing check
     pieces_to_take_checker = checked_by(opponent_color, opponent_position)
     viable_moves = Hash.new { |h, k| h[k] = [] }
     pieces_to_take_checker.each do |piece_position|
       viable_moves[piece_position] << opponent_position
     end
-    viable_moves #hash like [from]: [to1, to2, ... ]
+    viable_moves #hash {from piece1: [opponent_position], from piece2: [opponent_position] }
   end
 
-  #only matters if there is a SINGLE opponent piece producing the check
   def moves_that_intercept_opponent(opponent_color, opponent_position, king_position)
+    #another possibility is to get out of check by placing a piece between the opponent piece responsible for check and the king
+    #only works if the checking piece is not a knight
     opponent_slope =  slope(opponent_position[1], opponent_position[0], king_position[1], king_position[0])
     viable_moves = Hash.new { |h, k| h[k] = [] }
 
     board.each_with_index do |row, m|
       row.each_with_index do |piece, n|
-        next if piece.nil? || piece.color == opponent_color || piece.is_a?(King) #only checking for moves kings teammates can make
-        slopes = piece.slopes #all non-king pieces have a slopes property
-        slopes.each do |piece_slope| #slopes updated to have both postive and negative
+        next if piece.nil? || piece.color == opponent_color || piece.is_a?(King) 
+        slopes = piece.slopes 
+        slopes.each do |piece_slope| 
           intersection_pt = intersection(opponent_position[1], opponent_position[0], opponent_slope, n, m, piece_slope)  #[y, x] ie. [row, col]
           viable_moves[[m, n]] << intersection_pt if intercept_occurs_in_range?(opponent_slope, piece_slope, intersection_pt, king_position, opponent_position)
         end
       end
     end
+  end
+
+  def en_passant_rescue?(opponent_position)
+    start_pt, end_pt = en_passant
+    return true if [start_pt[0], end_pt[1]] == opponent_position
+    return false
   end
 
   def intercept_occurs_in_range?(slope1, slope2, intersection_pt, king_position, opponent_position)
