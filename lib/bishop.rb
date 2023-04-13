7e5387b4d25e72dbf1bb4277d4288dda77921fbb
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-13 13:48:03 -0400

    Refactor to add negative slopes to slopes property

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -8,7 +8,7 @@ class Bishop
   attr_reader :slopes, :color, :num
 
   def initialize(num = 1, color = "white")
-    @slopes = Set.new([1.0])
+    @slopes = Set.new([1.0, -1.0])
     @color = color
     @num = num
   end
@@ -27,8 +27,7 @@ class Bishop
 
   def valid_move?(board, start_idx, end_idx)
     slope = slope(start_idx[1], start_idx[0], end_idx[1], end_idx[0])
-    return false if slope.nil?
-    return false unless slopes.include?(slope.abs)
+    return false unless slopes.include?(slope)
     return false unless clear_non_vertical_path?(board, start_idx, end_idx, slope)
     true
   end
