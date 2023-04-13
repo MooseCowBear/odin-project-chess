1ca6c8a440c211abde5b0b9723f70472137c02b8
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-13 14:03:40 -0400

    Refactor so intersection returns point with row index before column index

--- lib/euclid.rb
+++ lib/euclid.rb
@@ -10,8 +10,7 @@ module Euclid
 
   #methods to find point of intersection between two lines
   def intersection(x1, y1, slope1, x2, y2, slope2)
-    #if no intersection, will return nil. else returns point of intersection as [x, y]
-    #will use separate method to check if the point of intersection is between opponent and king
+    #if no intersection, will return nil. else returns point of intersection as [y, x] bc we want it in that form
     return nil if slope1 == slope2
     if slope1.nil? 
       return find_intersection_vertical(x2, y2, slope2, x1)
@@ -25,12 +24,12 @@ module Euclid
   def find_intersection_vertical(non_vert_x, non_vert_y, non_vert_slope, vert_x)
     x = vert_x
     y = non_vert_y + non_vert_slope * (vert_x - non_vert_x)
-    [x, y] 
+    [y, x] 
   end
 
   def find_intersection_non_vertical(x1, y1, slope1, x2, y2, slope2)
     x = (slope1 * x1 - slope2 * x2 + y2 - y1) / (slope1 - slope2)
     y = y1 + slope1 * ((slope1 * x1 - slope2 * x2 + y2 - y1) / (slope1 - slope2)) - slope1 * x1
-    [x, y] #will need to convert to row, col, ie. [y, x] form
+    [y, x] 
   end
 end
