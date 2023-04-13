f593c1e3c1f0862b1eb6ca91698245304794afe1
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-13 06:03:00 -0400

    Remove extraneous on board check

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -1,25 +1,18 @@
 require_relative './euclid.rb'
 require_relative './path_checker.rb'
-require_relative './board_boundry.rb'
 
 class Bishop
   include Euclid
   include PathChecker
-  include BoardBoundry
 
-  attr_reader :slopes
+  attr_reader :slopes, :color
 
-  def initialize
+  def initialize(color = "white")
     @slopes = Set.new([1.0])
+    @color = color
   end
 
   def valid_move?(board, start_idx, end_idx)
-    correct_move?(board, start_idx, end_idx) && on_board?(board, end_idx)
-  end
-
-  private
-
-  def correct_move?(board, start_idx, end_idx)
     slope = slope(start_idx[1], start_idx[0], end_idx[1], end_idx[0])
     return false if slope.nil?
     return false unless slopes.include?(slope.abs)
