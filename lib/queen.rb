325ccd164ed0d69b45fc28a27fa75c38a74e25e3
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-13 13:48:14 -0400

    Refactor to add negative slopes to slopes property

--- lib/queen.rb
+++ lib/queen.rb
@@ -8,7 +8,7 @@ class Queen
   attr_reader :slopes, :color
 
   def initialize(color = "white")
-    @slopes = Set.new([nil, 1.0, 0.0]) #will check abs of slope
+    @slopes = Set.new([nil, 1.0, 0.0, -1.0]) #will check abs of slope
     @color = color
   end
 
@@ -22,7 +22,7 @@ class Queen
 
   def valid_move?(board, start_idx, end_idx)
     slope = slope(start_idx[1], start_idx[0], end_idx[1], end_idx[0])
-    return false unless slopes.include?(slope) || slopes.include?(slope.abs)
+    return false unless slopes.include?(slope)
 
     path_clear = slope.nil? ? clear_vertical_path?(board, start_idx, end_idx) : clear_non_vertical_path?(board, start_idx, end_idx, slope)
     return false unless path_clear
