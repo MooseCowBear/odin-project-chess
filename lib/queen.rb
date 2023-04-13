304890e8b0cec6c212ecbf485b0aba4c6e89c835
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-13 07:57:29 -0400

    Add to_s, get_start_position, color, remove extraneous on board check

--- lib/queen.rb
+++ lib/queen.rb
@@ -1,25 +1,26 @@
 require_relative './euclid.rb'
 require_relative './path_checker.rb'
-require_relative './board_boundry.rb'
 
 class Queen
   include Euclid
   include PathChecker 
-  include BoardBoundry
 
-  attr_reader :slopes
+  attr_reader :slopes, :color
 
-  def initialize
+  def initialize(color = "white")
     @slopes = Set.new([nil, 1.0, 0.0]) #will check abs of slope
+    @color = color
+  end
+
+  def to_s 
+    color == "white" ? "\u{2655}" : "\u{265B}"
+  end
+
+  def get_start_position
+    color == "black" ? [0, 3] : [7, 3]
   end
 
   def valid_move?(board, start_idx, end_idx)
-    correct_move?(board, start_idx, end_idx) && on_board?(board, end_idx)
-  end
-
-  private
-
-  def correct_move?(board, start_idx, end_idx)
     slope = slope(start_idx[1], start_idx[0], end_idx[1], end_idx[0])
     return false unless slopes.include?(slope) || slopes.include?(slope.abs)
 
