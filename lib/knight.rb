eb5b874a2b5d0cf58eb915dfdb0f34030b26f662
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-13 07:57:58 -0400

    Add to_s, get_start_position

--- lib/knight.rb
+++ lib/knight.rb
@@ -5,16 +5,29 @@ class Knight
   include Euclid
   include PathChecker
 
-  attr_reader :slopes, :distances
+  attr_reader :slopes, :distances, :color, :num
   
-  def initialize(color = "white")
+  def initialize(num = 1, color = "white")
     @slopes = Set.new([2.0, 0.5])
     @distances = Set.new([Math.sqrt(5)])
     @color = color
+    @num = num
+  end
+
+  def to_s 
+    color == "white" ? "\u{2658}" : "\u{265E}"
+  end
+
+  def get_start_position
+    if num == 1
+      color == "black" ? [0, 1] : [7, 1]
+    else
+      color == "black" ? [0, 6] : [7, 6]
+    end
   end
 
   def valid_move?(board, start_idx, end_idx)
-    correct_slope?(board, start_idx, end_idx) && correct_distance?(board, start_idx, end_idx)
+    correct_slope?(board, start_idx, end_idx) && correct_distance?(start_idx, end_idx)
   end
 
   private 
@@ -25,7 +38,7 @@ class Knight
     true
   end
 
-  def correct_distance?(board, start_idx, end_idx)
+  def correct_distance?(start_idx, end_idx)
     dist = distance(start_idx[1], start_idx[0], end_idx[1], end_idx[0])
     return false unless distances.include?(dist)
     true
