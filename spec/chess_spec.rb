65c1730d88bb8d0c10548254861dab6717fe58e8
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-14 14:32:25 -0400

    Add tests for checked by

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -55,4 +55,59 @@ describe Chess do
       end
     end
   end
+
+  #needed checked_by before castling, bc castling depends on it!!
+  define '#checked_by' do
+    let(:teammate) { double(color:"black") }
+    let(:opponent1) { double(color:"white")}
+    let(:opponent2) { double(color:"white") }
+    let(:board) { 
+      [
+        [nil, nil, nil, opponent1, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, "k", nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, opponent2, nil, nil, nil], 
+        [nil, nil, nil, nil, nil, nil, teammate, nil],
+        [nil, nil, nil, nil, nil, nil, nil, nil]
+      ]
+    }
+
+    context 'when opponent pieces that have valid moves to the king' do
+      let(:team_pos) { [6, 6] }
+      let(:opp1_pos) { [0, 3] }
+      let(:opp2_pos) { [5, 4] }
+      let(:king_pos) { [3, 3] }
+
+      before do
+        allow(:opponent1).to receive(:valid_move?).with(board, opp1_pos, king_pos).and_return(true)
+        allow(:opponent2).to receive(:valid_move?).with(board, opp2_pos, king_pos).and_return(true)
+        allow(:teammate).to receive(:valid_move?).with(board, team_pos, king_pos).and_return(true)
+      end 
+
+      it 'returns positions of checking opponents' do 
+        res = test_chess.checked_by("black", king_pos)
+        expect(res).to contain_exactly [0, 3], [5, 4]
+      end
+    end
+
+    context 'when there are no opponent pieces that have valid moves to the king' do
+      let(:team_pos) { [6, 6] }
+      let(:opp1_pos) { [0, 3] }
+      let(:opp2_pos) { [5, 4] }
+      let(:king_pos) { [3, 3] }
+
+      before do
+        allow(:opponent1).to receive(:valid_move?).with(board, opp1_pos, king_pos).and_return(false)
+        allow(:opponent2).to receive(:valid_move?).with(board, opp2_pos, king_pos).and_return(false)
+        allow(:teammate).to receive(:valid_move?).with(board, team_pos, king_pos).and_return(true)
+      end
+
+      it 'returns an empty list' do
+        res = test_chess.checked_by("black", king_pos)
+        expect(res).to eq([])
+      end
+    end
+  end
 end
