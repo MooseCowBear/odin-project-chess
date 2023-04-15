939f42f621063b8caae3e22fc43794a9e40136b1
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-15 13:20:43 -0400

    Add update defense method

--- lib/pin.rb
+++ lib/pin.rb
@@ -1,11 +1,16 @@
 class Pin
 
-  attr_reader :identity, :position, :defending_against, :defending_position 
+  attr_reader :identity, :position, :defending_position 
+  attr_accessor :defending_against
 
-  def initialize(identity, position, defending_against, defending_position)
+  def initialize(identity, position, defending_position)
     @identity = identity
     @position = position
-    @defending_against = defending_against
+    @defending_against = nil
     @defending_position = defending_position
+  end
+
+  def update_defense(opponent_pos)
+    self.defending_against = opponent_pos 
   end
 end
