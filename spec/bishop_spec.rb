9003fc63e8d3f29a10206096dcda4b466c64d540
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-17 10:08:25 -0400

    add tests for moves

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -48,4 +48,55 @@ describe Bishop do
       end
     end
   end
+
+  describe '#moves' do
+    subject(:test_bishop) { described_class.new }
+    let(:opponent) { double(color: "black") }
+    let(:teammate) { double(color: "white") }
+
+    context "when bishop's moves are constrained" do
+      let(:moves_board) { 
+        [
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, opponent, nil, nil, nil, nil], #opponent at 1, 3
+          [nil, nil, test_bishop, nil, nil, nil, nil, nil], #bishop at 2, 2
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [teammate, nil, nil, nil, teammate, nil, nil, nil], #teammates at 4, 0 and 4, 4
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+        ]
+      }
+
+      it 'returns expected moves up to and including opponent but excluding teammates' do
+        res = test_bishop.moves(moves_board, [2, 2])
+        value_to_check = res[[2, 2]]
+
+        expect(value_to_check).to match_array([[0, 0], [1, 1], [3, 3], [1, 3], [3, 1]])
+      end
+    end
+
+    context "when bishop's move are unconstrained" do
+      let(:moves_board) { 
+    
+        [
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil], 
+          [nil, nil, test_bishop, nil, nil, nil, nil, nil], #bishop at 2, 2
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+        ]
+      }
+
+      it 'returns every square on each diagonal' do
+        res = test_bishop.moves(moves_board, [2, 2])
+        value_to_check = res[[2, 2]]
+
+        expect(value_to_check).to match_array([[0, 0], [1, 1], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [1, 3], [0, 4], [3, 1], [4, 0]])
+      end
+    end
+  end
 end
