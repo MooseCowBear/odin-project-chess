2731e23b32775024c5b699e5e26cea152978c542
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-17 10:12:54 -0400

    Update tests

--- spec/path_checker_spec.rb
+++ spec/path_checker_spec.rb
@@ -2,11 +2,12 @@ require_relative '../lib/path_checker.rb'
 
 describe PathChecker do
   let(:dummy_class) { Class.new { extend PathChecker } }
+  let(:elem) { double(color: "black") }
   let(:test_board) { 
     [
-      ["x", nil, "x", nil],
-      [nil, nil, nil, "x"],
-      [nil, nil, "x", nil],
+      [elem, nil, elem, nil],
+      [nil, nil, nil, elem],
+      [nil, nil, elem, nil],
       [nil, nil, nil, nil]
     ]
   }
@@ -14,40 +15,40 @@ describe PathChecker do
   describe '#clear_non_vertical_path?' do
     context 'when there is a clear diagonal path' do
       it 'returns true' do
-        path = dummy_class.clear_non_vertical_path?(test_board, [0, 0], [2, 2], 1)
+        path = dummy_class.clear_non_vertical_path?("white", test_board, [0, 0], [2, 2], 1)
         expect(path).to be true
       end
     end
 
     context 'when the diagonal path is not clear' do
       it 'returns false' do
-        path = dummy_class.clear_non_vertical_path?(test_board, [0, 0], [3, 3], 1)
+        path = dummy_class.clear_non_vertical_path?("white", test_board, [0, 0], [3, 3], 1)
         expect(path).to be false
       end
 
       it 'returns false' do
-        path = dummy_class.clear_non_vertical_path?(test_board, [3, 3], [0, 0], 1) 
+        path = dummy_class.clear_non_vertical_path?("white", test_board, [3, 3], [0, 0], 1) 
         expect(path).to be false
       end
     end
 
     context 'when there is a clear horizontal path' do
       it 'returns true' do
-        path = dummy_class.clear_non_vertical_path?(test_board, [0, 0], [0, 2], 0) 
+        path = dummy_class.clear_non_vertical_path?("white", test_board, [0, 0], [0, 2], 0) 
         expect(path).to be true
       end
     end
 
     context 'when the horizontal path is not clear' do
       it 'returns false' do
-        path = dummy_class.clear_non_vertical_path?(test_board, [0, 0], [0, 3], 0)
+        path = dummy_class.clear_non_vertical_path?("white", test_board, [0, 0], [0, 3], 0)
         expect(path).to be false
       end
     end
 
     context 'when slope is negative' do
       it 'returns false' do
-        path = dummy_class.clear_non_vertical_path?(test_board, [1, 3], [3, 1], -1)
+        path = dummy_class.clear_non_vertical_path?("white", test_board, [1, 3], [3, 1], -1)
         expect(path).to be false
       end
     end
@@ -56,19 +57,19 @@ describe PathChecker do
   describe '#clear_vertical_path?' do
     context 'when there is a clear path' do
       it 'returns true' do
-        path = dummy_class.clear_vertical_path?(test_board, [0, 2], [2, 2])
+        path = dummy_class.clear_vertical_path?("white", test_board, [0, 2], [2, 2])
         expect(path).to be true
       end
     end
 
     context 'when the path is not clear' do
       it 'returns false' do
-        path = dummy_class.clear_vertical_path?(test_board, [0, 2], [3, 2])
+        path = dummy_class.clear_vertical_path?("white", test_board, [0, 2], [3, 2])
         expect(path).to be false
       end
 
       it 'returns false' do
-        path = dummy_class.clear_vertical_path?(test_board, [3, 2], [0, 2]) 
+        path = dummy_class.clear_vertical_path?("white", test_board, [3, 2], [0, 2]) 
         expect(path).to be false
       end
     end
