a777dea4f94a436a84b3abb757077ff36f47252e
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-17 10:22:16 -0400

    debug moves, clean up valid move

--- lib/rook.rb
+++ lib/rook.rb
@@ -38,10 +38,13 @@ class Rook
       m = start_idx[0] + o[0]
       n = start_idx[1] + o[1]
       while on_board?([m, n])
-        if valid_move?(color, board, start_idx, [m, n])
+        if valid_move?(board, start_idx, [m, n])
           moves[start_idx] << [m, n] 
+          m += o[0]
+          n += o[1]
         else 
           break
+        end
       end
     end
     moves
@@ -50,8 +53,6 @@ class Rook
   def valid_move?(board, start_idx, end_idx)
     slope = slope(start_idx[1], start_idx[0], end_idx[1], end_idx[0])
     path_clear = slope.nil? ? clear_vertical_path?(color, board, start_idx, end_idx) : clear_non_vertical_path?(color, board, start_idx, end_idx, slope)
-
-    return false unless slopes.include?(slope) && path_clear 
-    true
+    slopes.include?(slope) && path_clear 
   end
 end
