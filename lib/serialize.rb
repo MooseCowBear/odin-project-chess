491200ade141b0b13880a5ccea827f765516936a
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-19 12:42:41 -0400

    debug saving, loading files

--- lib/serialize.rb
+++ lib/serialize.rb
@@ -3,22 +3,24 @@ require 'fileutils'
 module Serialize
 
   def save_game(game)
-    filename = make_filename
+    filename = make_filename(game)
     Dir.mkdir 'saves' unless Dir.exist? 'saves'
-    File.open('saves/' + filename, 'wb') {|f| f.write(Marshal.dump(game))}
+    File.open('saves/' + filename, 'w+') { |f| f.write(Marshal.dump(game)) } 
   end
   
   def make_filename(game)
-    name = game.played_on.strftime("%d/%m/%Y_%I:%M_%p")
-    '/' + name 
+    name = game.played_on.strftime("%d_%m_%Y_%I_%M_%p")
+    name + '.txt'
   end
   
   def load_games
-    files = Dir.glob("*.txt")
+    path =  File.expand_path('../saves', __dir__)
+    files = Dir["#{path}/*.txt"]
     games = []
     files.each do |f|
-      f_name = File.basename(f)
-      reconstructed = Marshal.load(File.binread(f_name))
+      pp f
+      reconstructed = Marshal.load(File.binread(f))
+      pp reconstructed
       games << reconstructed
     end
     games
@@ -26,7 +28,12 @@ module Serialize
 
   def unfinished_games
     games = load_games
-    games.select { |game| game.winner.nil? && game.num_moves < 75 && game.stalemate = false}
+    games.select do |game| 
+      pp game.winner.nil?
+      pp game.num_moves < 75
+      pp game.stalemate == false
+      game.winner.nil? && game.num_moves < 75 && game.stalemate == false
+    end
   end
 
   def display_game_choices(games)
