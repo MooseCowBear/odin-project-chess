eed2e83252f052c93809d7207b31fd7412e1c7ec
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-09 08:37:55 -0500

    remove default of nil for captures because will have a capture

--- lib/moves/enpassant.rb
+++ lib/moves/enpassant.rb
@@ -3,7 +3,7 @@ require_relative "./move.rb"
 class EnPassant < Move
   attr_reader :additional_from
 
-  def initialize(from:, to:, piece:, additional_from:, captures: nil)
+  def initialize(from:, to:, piece:, additional_from:, captures:)
     super(from: from, to: to, piece: piece, captures: captures)
     @additional_from = additional_from
   end
