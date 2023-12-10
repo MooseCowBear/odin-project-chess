fb83adcb3c0e7e8df73647628ce98b6c79c04c1a
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-10 07:09:22 -0500

    update pin class, no longer need to hold positions since pieces hold them

--- lib/pin.rb
+++ lib/pin.rb
@@ -1,16 +1,8 @@
 class Pin
+  attr_reader :piece, :attacker
 
-  attr_reader :identity, :position, :defending_position 
-  attr_accessor :defending_against
-
-  def initialize(identity, position, defending_position)
-    @identity = identity
-    @position = position
-    @defending_against = nil
-    @defending_position = defending_position
-  end
-
-  def update_defense(opponent_pos)
-    self.defending_against = opponent_pos 
+  def initialize(piece:, attacker:)
+    @piece = piece
+    @attacker = attacker
   end
 end
