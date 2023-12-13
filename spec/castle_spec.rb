e6a5bc7a06926c4cb1d7dcfd65bf5d126c437a1d
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-13 07:39:55 -0500

    change hash syntax to be consistent

--- spec/castle_spec.rb
+++ spec/castle_spec.rb
@@ -10,10 +10,10 @@ describe Castle do
     it "updates the board 4 times" do
       allow(test_king).to receive(:position=)
       allow(test_king).to receive(:moved=)
-      expect(board).to receive(:update).with({ :position => [0, 0], :value => nil })
-      expect(board).to receive(:update).with({ :position =>[1, 1], :value => test_king })
-      expect(board).to receive(:update).with({ :position => [2, 2], :value => nil })
-      expect(board).to receive(:update).with({ :position => [3, 3], :value => test_rook })
+      expect(board).to receive(:update).with({ position: [0, 0], value: nil })
+      expect(board).to receive(:update).with({ position: [1, 1], value: test_king })
+      expect(board).to receive(:update).with({ position: [2, 2], value: nil })
+      expect(board).to receive(:update).with({ position: [3, 3], value: test_rook })
       test_castle.execute(board)
     end
   end
