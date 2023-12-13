b220acd1635898ea861f306bd8189bee4f4af002
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-13 17:04:29 -0500

    move move getting to own class, add tests

---
+++ lib/move_generators/move_generator.rb
@@ -0,0 +1,115 @@
+require_relative "../modules/square_checker.rb"
+require_relative "../modules/euclid.rb"
+require_relative "./castle_generator.rb"
+
+class MoveGenerator
+  include SquareChecker
+  include Euclid
+
+  attr_reader :king, :board, :checks, :last_move
+  attr_accessor :enpassant
+
+  def initialize(king, board, checks, last_move)
+    @king = king
+    @board = board
+    @checks = checks
+    @last_move = last_move
+  end
+
+  def self.for(king:, board:, checks:, last_move:) 
+    case checks.length
+    when 0
+      MoveGenerator
+    when 1
+      SingleCheckMoveGenerator
+    else
+      MultiCheckMoveGenerator
+    end.new(king, board, checks, last_move) 
+  end
+
+  def moves
+     king_moves + get_enpassant + non_king_moves + CastleGenerator.new(king, board).castles
+  end
+
+  def king_moves
+    king.valid_moves.filter { |move| unchecked_square?(square: move.to, board: board, alliance: king) }
+  end
+
+  def get_enpassant
+    @enpassant = []
+    if last_move.check_enpassant?
+      neighbors = board.column_neighbors(move: last_move).filter do |n|
+        n.opponent?(last_move.piece) && n.promotable? 
+      end
+      neighbors.each do |neighbor| 
+        to = [last_move.to[0] + neighbor.direction, last_move.to[1]] 
+        @enpassant << EnPassant.new(from: neighbor.position, to: to, piece: neighbor, additional_from: last_move.to, captures: last_move.piece)
+      end
+    end
+    @enpassant
+  end
+
+  def non_king_moves
+    pins = PinFinder.new(board, king).get_pins
+    8.times do |row|
+      8.times do |col|
+        moves << piece_moves(board.get_piece([row, col]), pins)
+      end
+    end
+    moves
+  end
+
+  def piece_moves(piece, pins) 
+    index = pins.index { |p| p.piece == piece }
+    if index 
+      pin_moves(pins[index].piece)
+    elsif piece && piece != king # not ideal
+      piece.valid_moves(from: piece.position, board: board) 
+    else
+      []
+    end
+  end
+
+  def pin_moves(pin)
+    moves = []
+    squares_in_range(pin.attacker).each do |square|
+      if square == piece.position
+        next
+      elsif pin.valid_move?(to: square, from: pin.position, board: board)
+        moves << Move.new(to: square, from: pin.position, piece: pin.piece, captures: board.get_piece(square))
+      elsif enpassant.any? { |ep| ep.piece == pin.piece && ep.to == square } 
+        moves << enpassant.select { |ep| ep.piece && ep.to == square }.first
+      end
+    end 
+    moves
+  end
+
+  def squares_in_range(opponent) 
+    squares = []
+    slope = slope(king.position, opponent.position) 
+
+    row, col = king.position 
+
+    if slope.nil?
+      dir = king.position[0] < opponent.position[0] ? 1 : -1
+      until row == opponent.position[0]
+        row += dir
+        squares << [row, col]
+      end
+    else
+      dir = king.position[1] < opponent.position[1] ? 1 : -1
+      until col == opponent.position[1]
+        col += dir
+        row += slope * dir
+        squares << [row.to_i, col.to_i]
+      end
+    end
+    squares 
+  end 
+end
+
+class MultiCheckMoveGenerator < MoveGenerator
+end
+
+class SingleCheckMoveGenerator < MoveGenerator
+end
---
+++ spec/move_generator_spec.rb
@@ -0,0 +1,312 @@
+require_relative "../lib/move_generators/move_generator.rb"
+require_relative "../lib/pin/pin_finder"
+require_relative "../lib/move_generators/castle_generator"
+require_relative "../lib/moves/enpassant"
+
+describe MoveGenerator do
+  describe ".for" do
+    it "creates a MultiCheckMoveGenerator when number of checks > 1" do
+      test_generator = 
+        MoveGenerator.for(
+          king: double(color: "white", position: [2, 2]), 
+          board: double(), 
+          checks: [double(color: "black", position: [1, 1]), double(color: "black", position: [4, 2])], 
+          last_move: double()
+        ) 
+      expect(test_generator.class.name).to eq("MultiCheckMoveGenerator")
+    end
+
+    it "creates a SingleCheckMoveGenerator when number of checks = 1" do
+      test_generator = 
+        MoveGenerator.for(
+          king: double(color: "white", position: [2, 2]), 
+          board: double(), 
+          checks: [double(color: "black", position: [4, 2])], 
+          last_move: double()
+        ) 
+
+      expect(test_generator.class.name).to eq("SingleCheckMoveGenerator")
+    end
+
+    it "creates a MoveGenerator when number of checks = 0" do
+      test_generator = 
+        MoveGenerator.for(
+          king: double(color: "white", position: [2, 2]), 
+          board: double(), 
+          checks: [], 
+          last_move: double()
+        ) 
+
+      expect(test_generator.class.name).to eq("MoveGenerator")
+    end
+  end
+
+  describe "#moves" do
+    it "combines king moves, non king moves, en passants, and castles" do
+      test_generator = described_class.new(
+            double(color: "white", position: [2, 2]), 
+            double(), 
+            [], 
+            double()
+          )
+
+      one = double("one")
+      two = double("two")
+      three = double("three")
+      four = double("four")
+
+      allow(test_generator).to receive(:get_enpassant).and_return([one])
+      allow_any_instance_of(CastleGenerator).to receive(:castles).and_return([two])
+      allow(test_generator).to receive(:king_moves).and_return([three])
+      allow(test_generator).to receive(:non_king_moves).and_return([four])
+      
+      res = test_generator.moves
+      expect(res.length).to eq(4)
+      expect(res.include?(one)).to be(true)
+      expect(res.include?(two)).to be(true)
+      expect(res.include?(three)).to be(true)
+      expect(res.include?(four)).to be(true)
+    end
+  end
+
+  describe "#king_moves" do
+    it "returns king moves that are to squares that are not in check" do
+      test_generator = described_class.new(
+            double(color: "white", position: [2, 2]), 
+            double(), 
+            [], 
+            double())
+
+      move_one = double(to: [1, 1])
+      move_two = double(to: [1, 2])
+      test_king_moves = [move_one, move_two]
+      allow(test_generator.king).to receive(:valid_moves).and_return(test_king_moves)
+      allow(test_generator).to receive(:unchecked_square?).and_return(true, false)
+
+      res = test_generator.king_moves
+      expect(res.length).to eq(1)
+      expect(res.first).to be(move_one)
+    end
+  end
+
+  describe "#get_enpassant" do
+    it "returns en passant moves if they are available" do
+      test_last_move = double(to: [4, 2], from: [6, 2], piece: double(color: "white", position: [4, 2], promotable: true))
+      test_generator = described_class.new(
+        double(color: "white", position: [2, 2]), 
+            double(), 
+            [], 
+            test_last_move
+      )
+      one = double("one", position: [1, 1])
+      two = double("two", position: [2, 2])
+      allow(test_last_move).to receive(:check_enpassant?).and_return(true)
+      allow(test_generator.board).to receive_message_chain(:column_neighbors, :filter).and_return([one, two])
+      allow(one).to receive(:direction).and_return(1)
+      allow(two).to receive(:direction).and_return(1)
+      allow(EnPassant).to receive(:new).and_return(double("ep"))
+
+      res = test_generator.get_enpassant
+      expect(res.length).to eq(2)
+    end
+
+    it "returns an empty array if last move returns false for check_enpassant?" do
+      test_last_move = double(to: [4, 2], from: [6, 2], piece: double(color: "white", position: [4, 2], promotable: true))
+      test_generator = described_class.new(
+        double(color: "white", position: [2, 2]), 
+            double(), 
+            [], 
+            test_last_move
+      )
+      allow(test_last_move).to receive(:check_enpassant?).and_return(false)
+
+      res = test_generator.get_enpassant
+      expect(res).to eq([])
+    end
+
+    it "returns an empty array if no column neighbors" do
+      test_last_move = double(to: [4, 2], from: [6, 2], piece: double(color: "white", position: [4, 2], promotable: true))
+      test_generator = described_class.new(
+        double(color: "white", position: [2, 2]), 
+            double(), 
+            [], 
+            test_last_move
+      )
+      allow(test_last_move).to receive(:check_enpassant?).and_return(true)
+      allow(test_generator.board).to receive_message_chain(:column_neighbors, :filter).and_return([])
+
+      res = test_generator.get_enpassant
+      expect(res).to eq([])
+    end
+  end
+
+  describe "#piece_moves" do
+    context "when piece is not a pin" do
+      it "calls piece moves" do
+        test_generator = described_class.new(
+          double(color: "white", position: [2, 2]), 
+              double(), 
+              [], 
+              double()
+        )
+        test_pins = double()
+        test_piece = double(position: [4, 4])
+        allow(test_pins).to receive(:index).and_return(nil)
+
+        expect(test_piece).to receive(:valid_moves)
+        test_generator.piece_moves(test_piece, test_pins)
+      end
+    end
+
+    context "when piece is a pin" do
+      it "calls pin moves" do
+        test_generator = described_class.new(
+          double(color: "white", position: [2, 2]), 
+              double(), 
+              [], 
+              double()
+        )
+        test_piece = double(position: [4, 4])
+        test_pin = double(piece: test_piece)
+        test_pins = [test_pin]
+        
+        expect(test_generator).to receive(:pin_moves)
+        test_generator.piece_moves(test_piece, test_pins)
+      end
+    end
+
+    context "when piece is king" do
+      it "returns empty array" do
+        test_king = double(color: "white", position: [2, 2])
+        test_generator = described_class.new(
+          test_king, 
+          double(), 
+          [], 
+          double()
+        )
+
+        test_pins = double()
+        allow(test_pins).to receive(:index).and_return(nil)
+
+        expect(test_generator.piece_moves(test_king, test_pins))
+      end
+    end
+  end
+
+  describe "#pin_moves" do
+    
+  end
+
+  describe "#non_king_moves" do
+  end
+
+  describe "#squares_in_range" do
+    context "for a horizontal line between the king and opponent" do
+      it "returns squares that include opponent square and exclude king when king column lower" do
+        test_generator = described_class.new(
+          double(color: "white", position: [1, 2]), 
+              double(), 
+              [], 
+              double
+          )
+        test_opponent = double(position: [1, 6])
+        allow(test_generator).to receive(:slope).and_return(0)
+
+        res = test_generator.squares_in_range(test_opponent)
+        expected = [[1, 3], [1, 4], [1, 5], [1, 6]]
+        expected.each do |sq|
+          expect(res.include?(sq)).to be(true)
+        end
+      end
+
+      it "returns squares that include opponent square and exclude king when king column higher" do
+        test_generator = described_class.new(
+          double(color: "white", position: [1, 6]), 
+              double(), 
+              [], 
+              double
+          )
+        test_opponent = double(position: [1, 2])
+        allow(test_generator).to receive(:slope).and_return(0)
+
+        res = test_generator.squares_in_range(test_opponent)
+        expected = [[1, 3], [1, 4], [1, 5], [1, 2]]
+        expected.each do |sq|
+          expect(res.include?(sq)).to be(true)
+        end
+      end
+    end
+
+    context "for a diagonal line between king and opponent" do
+      it "returns squares that include opponent square and exclude king when slope is negative" do
+        test_generator = described_class.new(
+          double(color: "white", position: [3, 3]), 
+              double(), 
+              [], 
+              double
+          )
+        test_opponent = double(position: [1, 5])
+        allow(test_generator).to receive(:slope).and_return(-1)
+
+        res = test_generator.squares_in_range(test_opponent)
+        expected = [[2, 4], [1, 5]]
+        expected.each do |sq|
+          expect(res.include?(sq)).to be(true)
+        end
+      end
+
+      it "returns squares that include opponent square and exclude king when slope is positive" do
+        test_generator = described_class.new(
+          double(color: "white", position: [3, 3]), 
+              double(), 
+              [], 
+              double
+          )
+        test_opponent = double(position: [4, 4])
+        allow(test_generator).to receive(:slope).and_return(1)
+
+        res = test_generator.squares_in_range(test_opponent)
+        expected = [[4,4]]
+        expected.each do |sq|
+          expect(res.include?(sq)).to be(true)
+        end
+      end
+    end
+
+    context "for a vertical line between king and opponent" do
+      it "returns squares that include opponent square and exclude king when king is lower row" do
+        test_generator = described_class.new(
+          double(color: "white", position: [3, 3]), 
+              double(), 
+              [], 
+              double
+          )
+        test_opponent = double(position: [0, 3])
+        allow(test_generator).to receive(:slope).and_return(nil)
+
+        res = test_generator.squares_in_range(test_opponent)
+        expected = [[2, 3], [1, 3], [0, 3]]
+        expected.each do |sq|
+          expect(res.include?(sq)).to be(true)
+        end
+      end
+
+      it "returns squares that include opponent square and exclude king when king is higher row" do
+        test_generator = described_class.new(
+          double(color: "white", position: [0, 3]), 
+              double(), 
+              [], 
+              double
+          )
+        test_opponent = double(position: [3, 3])
+        allow(test_generator).to receive(:slope).and_return(nil)
+
+        res = test_generator.squares_in_range(test_opponent)
+        expected = [[2, 3], [1, 3], [3, 3]]
+        expected.each do |sq|
+          expect(res.include?(sq)).to be(true)
+        end
+      end
+    end
+  end
+end
