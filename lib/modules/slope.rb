8578700a598563bf71bc78ae85338ffd445b7a61
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-14 12:32:47 -0500

    remove comment

--- lib/modules/slope.rb
+++ lib/modules/slope.rb
@@ -1,7 +1,7 @@
 module Slope
   def slope(from, to) 
-    row1, col1 = from # row, col
-    row2, col2 = to # row, col
+    row1, col1 = from 
+    row2, col2 = to 
     return nil if (col2.to_f - col1.to_f) == 0
     return (row2.to_f - row1.to_f) / (col2.to_f - col1.to_f)
   end
