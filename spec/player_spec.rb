3d323cc9be6cb6b465d35b43e75386be9b5806ea
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-14 12:02:53 -0500

    update player class, tests

---
+++ spec/player_spec.rb
@@ -0,0 +1,18 @@
+require_relative "../lib/players/player"
+
+describe Player do
+  subject(:test_player) { described_class.new(name: "alice") }
+
+  describe "#name" do
+    it "returns its name" do
+      expect(test_player.name).to eq("alice")
+    end
+  end
+
+  describe "#move" do
+    it "raises exception" do
+      msg = "This method needs to be defined in the subclass"
+      expect { test_player.move([]) }.to raise_error(msg)
+    end
+  end 
+end
