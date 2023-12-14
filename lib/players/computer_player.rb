08676d09147dbfb776e3a571b6b1f02ab6ad46f9
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-14 08:23:37 -0500

    refactor computer player

--- lib/players/computer_player.rb
+++ lib/players/computer_player.rb
@@ -2,30 +2,10 @@ class ComputerPlayer
   attr_reader :name
 
   def initialize
-    @name = "Hal"
+    super(name: "Hal")
   end
 
-  def make_move(check, unspecial_moves, castles, en_passant)
-    moves = moves_to_arr(check, unspecial_moves, castles, en_passant)
-    
-    moves.sample
-  end
-
-  def moves_to_arr(check, unspecial_moves, castles, en_passant)
-    arr = []
-    unspecial_moves.each do |k, v|
-      v.each do |elem|
-        arr << [k, elem]
-      end
-    end
-    en_passant.each do |elem|
-      arr << [elem.from , elem.to]
-    end
-    unless check 
-      castles.each do |elem|
-        arr << [elem.king_start, elem.king_end]
-      end
-    end
-    arr
+  def move(moves) # this is returning the move already in form that chess wants
+    move = moves.sample
   end
 end
