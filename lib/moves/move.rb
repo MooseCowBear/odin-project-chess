823d7256b89e5209a55dfa78791e990ac2ffe623
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-17 08:49:10 -0500

    add to_s method

--- lib/moves/move.rb
+++ lib/moves/move.rb
@@ -22,4 +22,17 @@ class Move
   def check_enpassant?
     piece.promotable? && (from[0] - to[0]).abs == 2
   end
+
+  def to_s
+    "from #{convert(from)} to #{convert(to)}"
+  end
+
+  private 
+
+  def convert(position)
+    letter = (position[1] + 97).chr
+    num = 8 - position[0]
+
+    "#{letter}#{num}"
+  end
 end
