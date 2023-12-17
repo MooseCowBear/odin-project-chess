6fce99508b59c8dee189eefacad041641f3b6180
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-17 08:00:12 -0500

    add method to create correct type of players depending on single player or two player mode

--- lib/players/player.rb
+++ lib/players/player.rb
@@ -5,6 +5,20 @@ class Player
     @name = name
   end
 
+  def self.create(two_player)
+    puts "Enter a name for player one:"
+    name_one = gets.chomp
+    player_one = HumanPlayer.new(name: name_one)
+    player_two = ComputerPlayer.new
+
+    if two_player
+      puts "Enter a name for player two:" 
+      name_two = gets.chomp
+      player_two = HumanPlayer.new(name: name_two)
+    end
+    [player_one, player_two]
+  end
+
   def move(moves)
     raise Exception.new "This method needs to be defined in the subclass"
   end
