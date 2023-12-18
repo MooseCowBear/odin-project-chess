9eebbad09ba4617196ba0ce92b82820246323f26
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-12-18 15:23:53 -0500

    add requires

--- lib/modules/setup.rb
+++ lib/modules/setup.rb
@@ -1,3 +1,12 @@
+require_relative "../players/Player"
+require_relative "../pieces/king"
+require_relative "../pieces/queen"
+require_relative "../pieces/bishop"
+require_relative "../pieces/rook"
+require_relative "../pieces/knight"
+require_relative "../pieces/pawn"
+require_relative "../board"
+
 module Setup 
   def setup
     get_players(two_player_mode?)
